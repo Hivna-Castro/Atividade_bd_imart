623dcfda7289e8567539d233962d4b188cc3d83c
Matheus Gois <mattheusgoiis@gmail.com>
2024-08-12 18:36:17 -0300

    just doing some querries

--- consultas.sql
+++ consultas.sql
@@ -1,12 +1,24 @@
 # consulta com ALL
+SELECT * FROM addresses;
 # consulta com DISTINCT
+SELECT DISTINCT state FROM addresses;
 # consulta com NULL
+SELECT * FROM addresses WHERE state IS NULL;
 
 # UPDATE
+UPDATE addresses SET state = 'CE', city = 'Fortaleza' WHERE state = 'SP';
 # DELETE
+
+DELETE FROM users WHERE name = 'Maria Souza';
 # INSERT
+INSERT INTO users (name, email, password_digest, role, market_id, status, created_at, updated_at)
+VALUES 
+('Maria Souza', 'maria@supermercadorio.com', 'senha_hash_2', 'manager', (SELECT id FROM markets WHERE name = 'Supermercado do Rio'), true, NOW(), NOW());
 
 # Consultas aninhadas
+SELECT * FROM users WHERE market_id = (SELECT id FROM markets WHERE name = 'Supermercado do Rio');
 # Trigger
+
 # View
+
 # Procedimentos armazenados
